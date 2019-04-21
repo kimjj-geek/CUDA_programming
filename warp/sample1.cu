e5d31d82340f07e51a58b91e1d4221b6cd214d93
kimjj.geek <kimjj.geek@sk.com>
2019-04-21 23:25:54 +0000

    CHECK is added to check cuda API return value

--- warp/sample1.cu
+++ warp/sample1.cu
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "../common/common.h"
 
 int main (int argc, char** argv)
 {
 	int count;
-	cudaGetDeviceCount(&count);
+	CHECK(cudaGetDeviceCount(&count));
 
 	for (int i = 0; i < count; i++) {
 		cudaDeviceProp prop;
-		cudaGetDeviceProperties(&prop, i);
+		CHECK(cudaGetDeviceProperties(&prop, i));
 
 		printf ("device #%d/%d\n", i+1, count);
  		printf ("    name: %s\n", prop.name);
