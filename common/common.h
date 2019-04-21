16a6511280b8e1691e7f1d4e41ffbc9a29f37576
kimjj.geek <kimjj.geek@sk.com>
2019-04-21 23:24:25 +0000

    initial commit

---
+++ common/common.h
@@ -0,0 +1,16 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define CHECK(call)                                                            \
+{                                                                              \
+    const cudaError_t error = call;                                            \
+    if (error != cudaSuccess)                                                  \
+    {                                                                          \
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);                 \
+        fprintf(stderr, "code: %d, reason: %s\n", error,                       \
+                cudaGetErrorString(error));                                    \
+    }                                                                          \
+}
+
+#endif
+
